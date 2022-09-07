36f17a8574e0f7a416103f2abb6e9d9e9d5b5b8e
Jonas <jonaschinagorom@gmail.com>
2022-09-07 15:28:33 +0200

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,30 @@
 import "./App.css";
+import Cart from "./pages/cart/Cart";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Product from "./pages/product/Product";
+import ProductList from "./pages/productList/ProductList";
+import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,Navigate,
+  Route
+} from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const user = useSelector((state)=>state.user.currentUser)
   return (
-    
-      <Home />
-    
+    <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/Products" element={<ProductList/>} />
+          <Route path="/product/:id" element={<Product/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={user ? <Navigate to="/" /> : <Login />}/>
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+    </Router>
   );
 }
 
